d4c826e83217adf59b7d0f3f82f77062f7a3b47c
Francesco Camporese <61165412+francescoCamporese@users.noreply.github.com>
2020-02-23 23:08:11 +0100

    Initial commit

---
+++ SharpSolver2.3/src/Impl.fs
@@ -0,0 +1,135 @@
+﻿(*
+ * SharpSolver - Progetto di Programmazione e Calcolo a.a. 2018-19
+ * Impl.fsi: implementazioni degli studenti
+ * (C) 2018 Alvise Spano' @ Universita' Ca' Foscari di Venezia
+ *)
+
+module SharpSolver.Impl
+
+open Absyn
+open Prelude
+open System
+open System.Runtime.Remoting.Metadata.W3cXsd2001
+open System.Numerics
+
+let rationalize (x : float) : rational = //partendo da denominatore = 10, moltiplico x e denominatore finchè la parte non intera di x non diventa 0
+    if x = 0.0 then //caso x = 0: numeratore e denominatore saranno 0
+        rational (0 , 0)
+    else if x % (float(int (x))) = 0.0 then //se x ha come parte decimale 0 teoricamente è un intero: restituisco n = |x| e d = 1
+        rational (int(x) , 1)
+    else
+        let mutable y = x
+        let mutable n = 0 
+        let mutable d = 1
+        while (y % (float(int (y)))) <> 0.0 do //finchè ho qualcosa dopo la virgola, diverso da 0, moltiplico per 10
+            y <- y * 10.0
+            n <- int(y)
+            d <- d * 10
+        rational (n , d)
+                                            
+
+let monomial_degree (m : monomial) : int = //so che un monomial contiene una coppia (coefficiente, grado)
+    match m with
+    |Monomial (coeff , deg) -> match coeff with
+                               |coeff when coeff = rational 0 -> 0
+                               |_ -> deg
+
+let monomial_negate (m : monomial) : monomial = //restituisco un monomial col coefficiente di segno negato
+    match m with
+    |Monomial (coeff , deg) -> Monomial (-coeff , deg)
+
+let polynomial_degree (p : polynomial) : int = //il grado di un polinomio è il grado massimo tra quelli dei suoi monomi
+    let mutable maxDeg = 0
+    let rec aux p = //utilizzo questa funzione per non dover rendere ricorsiva la funzione polynomial_degree 
+        match p with
+        |[] -> maxDeg
+        |x :: xs -> match x with
+                    |Monomial (coeff , deg) -> if (deg > maxDeg && coeff <> rational 0) then //il monomio ha grado massimo
+                                                   maxDeg <- deg
+                                                   aux xs
+                                               else
+                                                   aux xs
+    match p with
+    |(Polynomial mon_list) -> aux mon_list //richiamo aux sul contenuto del polynomial, cioè su una lista di monomial
+
+let polynomial_negate (p : polynomial) : polynomial = //un polinomio negato è un polinomio costituito dai suoi monomi con i loro coefficienti di segno opposto
+    let mutable final_list = []
+    let rec auxB p =
+                    match p with
+                    |[] -> Polynomial final_list //restituisco un polinomio contenente la lista di monomi che ho creato
+                    |x :: xs -> (final_list <- final_list @ [monomial_negate x]) //concateno alla lista i monomi negati
+                                auxB xs
+    match p with
+    |(Polynomial mon_list) -> auxB mon_list
+
+let normalized_polynomial_degree (np : normalized_polynomial) : int =  
+    match np with
+    |(NormalizedPolynomial rat_arr) -> (rat_arr.Length) - 1 //(trovo la lunghezza - 1) dell' array di rational contenuto nel polinomio normalizzato perchè se è di n-esimo grado avrò n + 1 coefficienti
+
+let normalize (p : polynomial) : normalized_polynomial = 
+    let normalized_to_monomial (arr: rational []) = //restituisce una lista di monomi di tipo coefficiente*x^posizione
+        let mutable list = []
+        for i = 0 to (arr.Length - 1) do
+            list <- list @ [Monomial (arr.[i] , i)]
+        list
+    let rec aux p = 
+        let mutable arrCoeffs = Array.init ((polynomial_degree p) + 1) (fun _ -> rational 0) //inizializzo l' array di coefficienti di tipo rational con (grado del polinomio posizioni + 1)
+        let rec auxC p =
+            match p with
+            |[] -> NormalizedPolynomial arrCoeffs //costruisco un polinomio normalizzato contenente l' array di coefficienti
+            |x :: xs -> match x with
+                        |Monomial (coeff , deg) -> ((arrCoeffs.[monomial_degree x]) <- ((arrCoeffs.[monomial_degree x]) + coeff)) //ottengo il coefficiente di ogni monomio di un certo grado e lo sommo all' elemento dell' array in posizione uguale al grado del monomio
+                                                   auxC xs
+        match p with
+        |(Polynomial mon_list) -> auxC mon_list
+    in
+    match aux p with
+    |(NormalizedPolynomial np) -> aux (Polynomial (normalized_to_monomial np)) //chiamo aux su un polinomio costruito attorno ad una lista di monomi creata a partire dal polinomio normalizzato costruito utilizzando normalized_to_monomial
+
+
+
+let derive (p : polynomial) : polynomial = 
+    let mutable listOfMon = [] //lista che conterrà le derivate dei monomi del polinomio dato
+    let rec auxE p =
+        match p with
+        |[] -> Polynomial listOfMon
+        |x :: xs -> match x with
+                    |Monomial (coeff , deg) -> if (deg = 0 || coeff = rational 0) then //se il grado o il coefficiente sono 0 scarto il monomio altrimenti avrei zeri inutili nella stampa
+                                                auxE xs
+                                               else //derivata di a*x^n = n*a*x^(n - 1)
+                                                listOfMon <- listOfMon @ [Monomial (((rational deg) * coeff), (deg - 1))]
+                                                auxE xs
+    match p with
+    |(Polynomial mon_list) -> auxE mon_list
+
+let reduce (e : expr) : polynomial = //restituisce un polinomio partendo dall' espressione
+    let rec reducer e =
+        match e with
+        |(Poly pol) -> pol //se expr è un polinomio lo restituisco
+        |(Derive der) -> derive (reducer der) //se expr è una derivata, ripeto la funzione sulla derivata
+    reducer e
+
+let solve0 (np : normalized_polynomial) : bool =                            
+    match np with //il contenuto del polinomio normalizzato sarà un array di rational ma...
+    |(NormalizedPolynomial rat_el) -> rat_el.[0] = rational 0 //essendo il polinomio composto da un monomio di grado 0 avrò un rational che dirò se è uguale a 0 o no
+
+let solve1 (np : normalized_polynomial) : rational = //il risultato sarà -b/a, so che in pos. 0 ho b e in pos. 1 ho a
+    match np with
+    |(NormalizedPolynomial rat_el) -> (- (rat_el.[0])) / (rat_el.[1]) //avendo un array di rational basta fare il calcolo sugli elementi nelle relative posizioni
+                                                                                       
+let solve2 (np : normalized_polynomial) : (float * float option) option = 
+    match np with //so che il polinomio è un array costituito da 3 coefficienti rational
+    |NormalizedPolynomial [|c0 ; c1 ; c2|] -> let a = float c2 //converto i rational in float in modo da poter fare i calcoli
+                                              let b = float c1
+                                              let c = float c0
+                                              let delta = (b * b) - (4.0 * a * c) //assegno il valore del delta in modo da inserirlo comodamente nelle formule
+                                              match delta with //la quantità di soluzioni dipende dal delta
+                                              |delta when delta > 0.0 -> let x1 = ((- b) + (sqrt delta)) / (2.0 * a) //due soluzioni reali distinte
+                                                                         let x2 = ((- b) - (sqrt delta)) / (2.0 * a)
+                                                                         Some (x1 , Some x2)
+                                              |delta when delta = 0.0 -> let coincResult = (- b) / (2.0 * a) //due soluzioni reali coincidenti
+                                                                         Some (coincResult , None)
+                                              |delta when delta < 0.0 -> None //nessuna soluzione nei numeri reali
+
+
+
---
+++ SharpSolver2.3/src/Main.fs
@@ -0,0 +1,148 @@
+﻿(*
+ * SharpSolver - Progetto di Programmazione e Calcolo a.a. 2018-19
+ * Main.fs: console e codice main
+ * (C) 2018 Alvise Spano' @ Universita' Ca' Foscari di Venezia
+ *)
+
+module SharpSolver.Main
+
+open Microsoft.FSharp.Text.Lexing
+open Absyn
+open System
+open Prelude
+open Microsoft.FSharp.Text
+open SharpSolver
+open SharpSolver
+
+
+// funzioni di logging e printing
+//
+
+let hout hd fmt =
+    if not <| String.IsNullOrWhiteSpace hd then
+        printf "[%s]%s" hd (new String (' ', max 1 (Config.prefix_max_len - String.length hd)))
+        stdout.Flush ()
+    printfn fmt
+
+let chout col hd fmt =
+    let c = Console.ForegroundColor
+    Console.ForegroundColor <- col
+    Printf.kprintf (fun s -> hout hd "%s" s; Console.ForegroundColor <- c) fmt
+
+let out fmt = hout "" fmt
+let cout col fmt = chout col "" fmt
+
+let norm fmt = chout ConsoleColor.Yellow "norm" fmt
+let redux fmt = chout ConsoleColor.Magenta "redux" fmt
+let degree fmt = chout ConsoleColor.Cyan "degree" fmt //aggiunto io perchè mancava
+let sol fmt = chout ConsoleColor.Green "sol" fmt
+let ident fmt = chout ConsoleColor.Green "ident" fmt    
+let error fmt = chout ConsoleColor.Red "error" fmt   
+
+// interprete dei comandi e delle espressioni
+//
+
+let interpreter_loop () =
+    while true do
+        printf "\n%s" Config.prompt_prefix          // stampa il prompt
+        stdout.Flush ()                             // per sicurezza flusha lo stdout per vedere la stampa del prompt senza end-of-line
+        let input = Console.ReadLine ()             // leggi l'input scritto dall'utente
+        let lexbuf = LexBuffer<_>.FromString input  // crea un lexbuffer sulla stringa di input
+
+        // funzione locale per il pretty-printing degli errori
+        let localized_error msg =
+            let tabs = new string (' ', Config.prompt_prefix.Length + lexbuf.StartPos.Column)
+            let cuts = new string ('^', let n = lexbuf.EndPos.Column - lexbuf.StartPos.Column in if n > 0 then n else 1)
+            cout ConsoleColor.Yellow "%s%s\n" tabs cuts
+            error "error at %d-%d: %s" lexbuf.StartPos.Column lexbuf.EndPos.Column msg 
+        
+        // blocco con trapping delle eccezioni
+        try
+            let line = Parser.line Lexer.tokenize lexbuf    // invoca il parser sul lexbuffer usando il lexer come tokenizzatore
+            #if DEBUG
+            hout "absyn" "%+A" line
+            hout "pretty" "%O" line
+            #endif
+
+            // interpreta la linea in base al valore di tipo line prodotto dal parsing
+            match line with
+            | Cmd "help" ->
+                out "%s" Config.help_text
+
+            | Cmd ("quit" | "exit") ->
+                out "%s" Config.exit_text
+                exit 0
+
+            // TODO: se volete supportare altri comandi, fatelo qui (opzionale)
+            
+            | Cmd s -> error "unknown command: %s" s    // i comandi non conosciuti cadono in questo caso
+
+            // TODO: aggiungere qui sotto i pattern per i casi Expr ed Equ con relativo codice per, rispettivamente, normalizzare espressioni e risolvere equazioni
+            | Expr e1 -> 
+                redux "%O" (Impl.reduce e1) //applicazione di reduce all' espressione
+                norm "%O" (Impl.normalize (Impl.reduce e1)) //versione normalizzata del polinomio derivato
+                degree "%O" (Impl.normalized_polynomial_degree (Impl.normalize (Impl.reduce e1))) //grado della versione normalizzata del polinomio derivato
+
+            | Equ (e1 , e2) ->
+                let leftSide = Impl.reduce e1 //monomi a sinistra dell' uguale
+                let mutable rightSide = Impl.reduce e2 //variabile perchè dopo redux nego i monomi a destra dell' uguale perchè vanno spostati alla sua sinistra
+                
+                redux "%O = %O" leftSide rightSide //applicazione di reduce alle due espressioni
+                let mutable singleNormPol = Impl.normalize (Polynomial([Monomial (rational (0) , 0)])) //inizializzo singleNormPol
+                if leftSide = rightSide then //se ho un' uguaglianza che semplificata è 0 = 0
+                    singleNormPol <- Impl.normalize (Polynomial([Monomial (rational (0) , 0)]))//non lo modifico
+                else
+                    rightSide <- Impl.polynomial_negate(rightSide)
+
+                    let left = let unboxLs leftSide = leftSide //lista di monomi contenuti nel polinomio a sinistra dell' uguale
+                               match leftSide with
+                               |(Polynomial mon_list) -> unboxLs mon_list    
+                    let right = let unboxRs rightSide = rightSide //lista di monomi contenuti nel polinomio a destra dell' uguale
+                                match rightSide with
+                                |(Polynomial mon_list) -> unboxRs mon_list
+                
+                    singleNormPol <- Impl.normalize (Polynomial (left @ right)) //monomi a sinistra + (- monomi a destra)
+                norm "%O = 0" singleNormPol
+
+                let singleNormPolDeg = Impl.normalized_polynomial_degree (singleNormPol) //grado del polinomio normalizzato
+                degree "%O" singleNormPolDeg
+
+                match (singleNormPolDeg) with //in base al grado, stampa la stringa contentente il risultato dell' equazione costituita dal polinomio = 0
+                | 0 -> ident "%O" (Impl.solve0 singleNormPol)
+                | 1 -> out "Risultato: x = %O" (Impl.solve1 singleNormPol)
+                | 2 -> match (Impl.solve2 singleNormPol) with //in base alla soluzione faccio una stampa adeguata
+                       |Some (x1 , x2) when x2 <> None -> sol "x1 = %O e x2 = %O" x1 x2.Value //x2.value serve ad ottenere il valore float del secondo elemento
+                       |Some (x1 , x2) when x2 = None -> sol "x1 = x2 = %O" x1
+                       |None -> sol "L' equazione non ha soluzioni nei numeri reali"
+                | _ -> error "L' equazione non e' risolvibile perche' di grado superiore al secondo"
+
+            | _ -> raise (NotImplementedException (sprintf "unknown command or expression: %O" line))
+                   
+        // gestione delle eccezioni
+        with LexYacc.ParseErrorContextException ctx ->
+                let ctx = ctx :?> Parsing.ParseErrorContext<Parser.token>
+                localized_error (sprintf "syntax error%s" (match ctx.CurrentToken with Some t -> sprintf " at token <%O>" t | None -> ""))
+
+           | Lexer.LexerError msg -> localized_error msg 
+
+           | :? NotImplementedException as e -> error "%O" e
+        
+           | e -> localized_error e.Message
+
+
+// funzione main: il programma comincia da qui
+//
+
+[<EntryPoint>]
+let main _ = 
+    let code =
+        try
+            interpreter_loop ()                 // chiama l'interprete
+            0                                   // ritorna il codice di errore 0 (nessun errore) al sistema operativo se tutto è andato liscio
+        with e -> error "fatal error: %O" e; 1  // in caso di eccezione esce con codice di errore 1
+    #if DEBUG
+    Console.ReadKey () |> ignore                // aspetta la pressione di un tasto prima di chiudere la finestra del terminare 
+    #endif
+    code
+
+
